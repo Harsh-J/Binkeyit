d4eb65b43dee2b4a6986c8e8f771cca5973b8bc6
Harsh-J <harshjagarwal123@gmail.com>
2025-05-13 23:14:04 +0530

    login page error

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -63,6 +63,8 @@ const Login = () => {
         navigate("/");
       }
     } catch (error) {
+      toast.error(error)
+      console.log("login catch error",error)
       setIsLoginLoading(false);
       AxiosToastError(error);
     }
